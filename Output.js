92b837934d57f1554891f00133c62f1daaeabda4
SirSheeply <52301930+SirSheeply@users.noreply.github.com>
2025-08-02 17:22:34 +1000

    Reorganized the modifier Output.js for compatability with other scripts

--- Output.js
+++ Output.js
@@ -1,4 +1,27 @@
+// Your "Output" tab should look like this
+
+// Checkout the Guidebook examples to get an idea of other ways you can use scripting
+// https://help.aidungeon.com/scripting
+
+/* NOTE: This is the Main AI Dungeon Call
+ * Keeping this space clean will allow for greater compatibility with other script systems.
+ */
 const modifier = (text) => {
+  // Your other output modifier scripts go here (preferred)
+  text = DNDHash_output(text)
+  // Your other output modifier scripts go here (alternative)
+  return {text}
+}
+
+// Don't modify this part
+modifier(text)
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+function DNDHash_output(text) {
   if (state.show == null) return { text }
 
   var character = getCharacter()
