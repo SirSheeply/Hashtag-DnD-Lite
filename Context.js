1f5a6057139120ec059480efdac5f89be8718bcd
SirSheeply <52301930+SirSheeply@users.noreply.github.com>
2025-08-02 17:20:06 +1000

    Formatted context.js for script compatibility and readability

--- Context.js
+++ Context.js
@@ -1,5 +1,16 @@
+// Your "Context" tab should look like this
+
+// Checkout the Guidebook examples to get an idea of other ways you can use scripting
+// https://help.aidungeon.com/scripting
+
+/* NOTE: This is the Main AI Dungeon Call
+ * Keeping this space clean will allow for greater compatibility with other script systems.
+ */
 const modifier = (text) => {
-  return { text }
+  // Your other context modifier scripts go here (preferred)
+  // Your other context modifier scripts go here (alternative)
+  return {text}
 }
 
+// Don't modify this part
 modifier(text)
